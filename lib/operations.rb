217e7092d5e7efc61211a8bb87175cdee14039c5
Grace Tan <gracetan37@gmail.com>
2019-05-25 19:32:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+ if speed < 40
+   true
+ elsif speed > 60
+   true
+ else
+   false
+end
 end
 
 
-
+#multiple conditions on one line (condition 1 ? ifTrue " condition 2? ifTrue : ifFalse")
 def not_safe?(speed)
-	
+	speed < 40  ? true : speed > 60 ? true : false 
 end
 	
 
